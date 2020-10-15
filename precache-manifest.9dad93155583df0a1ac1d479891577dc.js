9b38790318572490b36f227523624a25b05a6926
Anatole Leterrier <anatoleleterrier@gmail.com>
2020-10-15 17:52:57 +0200

    Updates

---
+++ precache-manifest.9dad93155583df0a1ac1d479891577dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7ec372f5c167891ecb9a6c36225525",
+    "url": "/blindtest-spotify/index.html"
+  },
+  {
+    "revision": "cec2bf968e714343e156",
+    "url": "/blindtest-spotify/static/css/main.12217c1b.chunk.css"
+  },
+  {
+    "revision": "99291f00ffc51cf5bd8a",
+    "url": "/blindtest-spotify/static/js/2.98663aa6.chunk.js"
+  },
+  {
+    "revision": "8dfbd160926ae814ef4bd67a82331fc7",
+    "url": "/blindtest-spotify/static/js/2.98663aa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cec2bf968e714343e156",
+    "url": "/blindtest-spotify/static/js/main.027fce76.chunk.js"
+  },
+  {
+    "revision": "01c4d54207b3c1322762",
+    "url": "/blindtest-spotify/static/js/runtime-main.e8875b69.js"
+  },
+  {
+    "revision": "ed34bc26e2e33c49af23c6c44c6bdee9",
+    "url": "/blindtest-spotify/static/media/loading.ed34bc26.svg"
+  },
+  {
+    "revision": "6a5812940578a284a5256eab66eddac5",
+    "url": "/blindtest-spotify/static/media/logo.6a581294.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blindtest-spotify/precache-manifest.af1d10f296fb8bac425bec0e56195ae1.js"
+  "/blindtest-spotify/precache-manifest.9dad93155583df0a1ac1d479891577dc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blindtest-spotify/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
